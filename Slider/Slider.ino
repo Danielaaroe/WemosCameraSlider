e49762d588258499f6641a67873c6699e9583fd4
Daniel <daniel.silseth.aaroe@gmail.com>
2020-11-08 20:17:42 +0100

    Added http POST from Peter P's beginners guide to ESP8266.

--- Slider/Slider.ino
+++ Slider/Slider.ino
@@ -22,12 +22,12 @@ int speedDir = 0;
 
 //WEB FEATURES////////////////////////////////////////////////////
 //ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
-//ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
-//void handleRoot();              // function prototypes for HTTP handlers
-//void handleSetParameters();
-//void handleStart();
-//void handleStop();
-//void handleReturn();
+ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+void handleRoot();              // function prototypes for HTTP handlers
+void handleSetParameters();
+void handleStart();
+void handleStop();
+void handleReturn();
 
 const char *ssid = "Sliderfuck";
 const char *password = "password";
@@ -64,11 +64,16 @@ void setup()
  //WiFi.disconnect();
   WiFi.softAP(ssid, password);
   //Serial.println(WiFi.softAPIP());
-
+  server.on("/", HTTP_GET, handleRoot);        // Call the 'handleRoot' function when a client requests URI "/"
+  //server.on("/login", HTTP_POST, handleLogin); // Call the 'handleLogin' function when a POST request is made to URI "/login"
+  server.onNotFound(handleNotFound);           // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+  server.begin();                            // Actually start the server
+  Serial.println("HTTP server started");
 }
 
 void loop()
 {
+  server.handleClient(); 
   //unsigned long nextkeep = next;
   //Serial.println(nextkeep);
 
@@ -108,6 +113,23 @@ void loop()
 
 }
 
-//void doStep(){
-//  digitalWrite(stepPin,!digitalRead(stepPin)); //toggles step pin
-//  }
+void handleRoot() {                          // When URI / is requested, send a web page with a button to toggle the LED
+  server.send(200, "text/html", "<form action=\"/login\" method=\"POST\"><input type=\"text\" name=\"username\" placeholder=\"Username\"></br><input type=\"password\" name=\"password\" placeholder=\"Password\"></br><input type=\"submit\" value=\"Login\"></form><p>Try 'John Doe' and 'password123' ...</p>");
+}
+/*
+void handleLogin() {                         // If a POST request is made to URI /login
+  if( ! server.hasArg("username") || ! server.hasArg("password") 
+      || server.arg("username") == NULL || server.arg("password") == NULL) { // If the POST request doesn't have username and password data
+    server.send(400, "text/plain", "400: Invalid Request");         // The request is invalid, so send HTTP status 400
+    return;
+  }
+  if(server.arg("username") == "John Doe" && server.arg("password") == "password123") { // If both the username and the password are correct
+    server.send(200, "text/html", "<h1>Welcome, " + server.arg("username") + "!</h1><p>Login successful</p>");
+  } else {                                                                              // Username and password don't match
+    server.send(401, "text/plain", "401: Unauthorized");
+  }
+}
+*/
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
